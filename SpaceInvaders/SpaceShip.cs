6bd4f45d7a148dc038e0db5b7d9c201a6e0cdd59
Sangerwan <sangerwan@gmail.com>
2020-09-15 14:05:33 +0200

    SpaceShip

---
+++ SpaceInvaders/SpaceShip.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+
+namespace SpaceInvaders
+{
+    class SpaceShip : GameObject
+    {
+        double speedPixelPerSecond;
+
+        public Vecteur2D Position
+        {
+
+        }
+        
+        public override void Draw(Game gameInstance, Graphics graphics)
+        {
+            throw new NotImplementedException();
+        }
+
+        public override bool IsAlive()
+        {
+            throw new NotImplementedException();
+        }
+
+        public override void Update(Game gameInstance, double deltaT)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
